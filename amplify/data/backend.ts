c8861ba677e205e31c2dcd68220df2123a7f6c1f
Zags <kelvinwork16@gmail.com>
2024-11-16 08:24:03 -0500

    fourth commit

---
+++ amplify/data/backend.ts
@@ -0,0 +1,30 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
+
+const backend = defineBackend({
+  auth,
+  data,
+});
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.us-east-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "us-east-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+    ],
+    actions: ["bedrock:InvokeModel"],
+    
+  })
+);
